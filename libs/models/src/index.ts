0372c50875c99a3dbf6b5ba50710999e51f4a6eb
NicolasMura <contact@nicolasmura.fr>
2021-11-05 23:59:31 +0100

    Migrate to latest nx Add backend-api skeleton

--- libs/models/src/index.ts
+++ libs/models/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/interfaces/user.interface';
 export * from './lib/models/user.model';
 export * from './lib/models/login-response.model';
+export * from './lib/models/todo.model';
 export * from './lib/mocks/user.mock';
